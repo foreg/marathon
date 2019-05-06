ca963a5a55bdeef649093edf7d5ce60272c556f5
Zakharov98 <D.Zakharov98@yandex.ru>
2019-05-06 18:09:46 +0500

    add models: country, gender, runner

---
+++ marathon/Models/Country.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Marathon.Models
+{
+    [Table("country")]
+    public class Country
+    {
+        [Key]
+        [Required]
+        [MaxLength(3)]
+        [Column(TypeName = "char")]
+        public string CountryCode { get; set; }
+
+        [MaxLength(100)]
+        [Required]
+        public string CountryName { get; set; }
+
+        [MaxLength(100)]
+        [Required]
+        public string CountryFlag { get; set; }
+    }
+}
---
+++ marathon/Models/Runner.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Marathon.Models
+{
+    [Table("runner")]
+    public class Runner
+    {
+        [Key]
+        [Required]
+        public string RunnerId { get; set; }
+        [MaxLength(100)]
+        [Required]
+        public string Email { get; set; }
+
+        [MaxLength(100)]
+        [Required]
+        public string GenderName { get; set; }
+
+        public DateTime DateOfBirth { get; set; }
+
+        [MaxLength(3)]
+        [Column(TypeName = "char")]
+        [Required]
+        public string CountryCode { get; set; }
+
+
+        [ForeignKey("GenderName")]
+        public virtual Gender Gender { get; set; }
+        [ForeignKey("CountryCode")]
+        public virtual Country Country { get; set; }
+    }
+}
